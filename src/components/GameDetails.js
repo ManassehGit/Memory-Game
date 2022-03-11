63c7f6f803a6eb01374d76543707debd0f72443d
Manasseh <manasseh.sarfo@amalitech.org>
2022-03-11 05:03:17 -0800

    Update on the info for selected player modes

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import InfoBox from './InfoBox';
 
+let numberOfPlayers = 5;
+let playerGroup = [];
+for(let i=0; i<numberOfPlayers; i++){
+  playerGroup.push(i);
+}
+
+const infoSection = playerGroup.map(player => <InfoBox />)
 const GameDetails = () => {
   return (
     <div className='game-details'>
-      <InfoBox />
-      <InfoBox />
-      <InfoBox />
-      <InfoBox />
+      {infoSection}
     </div>
   )
 }
