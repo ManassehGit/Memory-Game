b7679a62a415f8efbc5aaaf4918322c5702d5d61
Manasseh <manasseh.sarfo@amalitech.org>
2022-03-11 04:31:37 -0800

    Update on the grid styling

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -3,10 +3,11 @@ import InfoBox from './InfoBox';
 
 const GameDetails = () => {
   return (
-    <div>
-        <p>
-            <InfoBox />
-        </p>
+    <div className='game-details'>
+      <InfoBox />
+      <InfoBox />
+      <InfoBox />
+      <InfoBox />
     </div>
   )
 }
--- src/components/GameSpace.js
+++ src/components/GameSpace.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const choice = 6;
+const columnStyle = {
+  gridTemplateColumns: `repeat(${choice}, 1fr)`
+}
 const grid = [];
-for(let i=1; i<=16; i++){
+for(let i=1; i<=choice**2; i++){
     grid.push(i);
 }
 
@@ -10,7 +14,7 @@ const GameSpace = props => {
     const tile = <div className="blue-04 tileStyle"><i className="fa-solid fa-cube iconStyle"></i></div>;
     const content = grid.map(item => tile);
   return (
-    <div className="gridStyle">
+    <div className="gridStyle" style={columnStyle}>
         {content}
     </div>
   )
