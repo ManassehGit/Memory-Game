ef0fc7d550e664d5b13b89d9540b562d01bb1c9a
__francis <francis.coffie@amalitech.org>
2022-03-14 21:16:42 +0000

    starting on gamr grid

--- src/components/GameGrid.js
+++ src/components/GameGrid.js
@@ -14,3 +14,21 @@ import {
 import { toggleModalGameEndVisibility } from '../store/modalsSlice';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { GAME_GRID_SIZES, GAME_THEMES, ICONS_ARR } from '../constants';
+const GameGrid = () => {
+    const gridSize = useSelector((state) => state.game.gridSize);
+    const gridTheme = useSelector((state) => state.game.gridTheme);
+    const gameElements = useSelector((state) => state.game.gameElements);
+    const lastTwoMoves = useSelector((state) => state.game.lastTwoMoves);
+    const numOfPlayers = useSelector((state) => state.game.numOfPlayers);
+    const dispatch = useDispatch();
+  
+    const canPlay = lastTwoMoves.length !== 2;
+  
+    useEffect(() => {
+      let timeout;
+      if (!canPlay) {
+        timeout = setTimeout(() => {
+          if (lastTwoMoves[0].value !== lastTwoMoves[1].value) {
+            dispatch(
+              hideGameElementsVisibility([lastTwoMoves[0], lastTwoMoves[1]])
+            );
