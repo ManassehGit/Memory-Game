857fbf0b26692d7056e7d88ba16a1bcf59df79b6
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-03-14 16:35:21 +0000

    files removed

--- Eshun_react/src/components/ModalWindow.js
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/ModalWindow.module.css';
-
-const ModalWindow = (props) => {
-  return <div className={styles['modal-window']}>{props.children}</div>;
-};
-
-export default ModalWindow;
