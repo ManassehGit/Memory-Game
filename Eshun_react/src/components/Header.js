b2a186955555230c8ab3759a3999b0bdc4494c53
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-03-14 16:41:11 +0000

    Create Header.js
    imported files

---
+++ Eshun_react/src/components/Header.js
@@ -0,0 +1,7 @@
+import styles from '../styles/Header.module.css';
+import OpenCloseMenuButton from './OpenCloseMenuButton';
+
+import Logo from './Logo';
+import RestartGameButton from './RestartGameButton';
+import StartNewGameButton from './StartNewGameButton';
+
