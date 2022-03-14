cadb0eed60ae0c21126b43de523d39c09a259c02
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-03-14 16:42:33 +0000

    Update Header.js
    functions updated

--- Eshun_react/src/components/Header.js
+++ Eshun_react/src/components/Header.js
@@ -5,3 +5,17 @@ import Logo from './Logo';
 import RestartGameButton from './RestartGameButton';
 import StartNewGameButton from './StartNewGameButton';
 
+const Header = () => {
+  return (
+    <header className={`${styles.header}`}>
+      <Logo color='#152938' />
+      <div className={`${styles.header__buttons}`}>
+        <OpenCloseMenuButton type='primary'>Menu</OpenCloseMenuButton>
+        <RestartGameButton type='primary'>Restart</RestartGameButton>
+        <StartNewGameButton type='secondary'>New Game</StartNewGameButton>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
