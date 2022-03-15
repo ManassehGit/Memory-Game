ca42a34a0497f31d98a309ca1d8d478e29bd5212
Patricia <patricia.bekyere@amalitech.org>
2022-03-15 00:13:40 -0700

    fourthFile added

--- src/components/GameButton.js
+++ src/components/GameButton.js
@@ -1,1 +1,23 @@
 import styles from '../styles/GameButton.module.css';
+
+const GameButton = (props) => {
+    return (
+      <div
+        className={`${styles['game-button']}`}
+        {...(!props.isVisible ? { onClick: props.onMoveMade } : {})}
+      >
+          <button
+        className={`${styles['game-button__cover']} ${
+          props.isVisible ? styles['game-button__cover--hidden'] : ''
+        }`}
+        ></button>
+        <div
+        className={`${styles['game-button__content']}  ${
+          props.isActive ? styles['game-button__content--active'] : ''
+        }`}
+      >
+        {props.children}
+      </div>
+    </div>
+  );
+};
