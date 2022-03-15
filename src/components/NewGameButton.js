da026261a351e9a0b80c2f868dd26850947b5644
Patricia <patricia.bekyere@amalitech.org>
2022-03-14 23:56:16 -0700

    first Commit

--- src/components/NewGameButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NewGameButton = () => {
-  return (
-    <div className='button gray-02'>New Game</div>
-  )
-}
-
-export default NewGameButton
--- src/components/RestartButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ResetButton = () => {
-  return (
-    <div className='button orange white-text'>Restart</div>
-  )
-}
-
-export default ResetButton
