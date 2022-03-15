04921157236179a431247b307971bb8f00c32f43
Manasseh <manasseh.sarfo@amalitech.org>
2022-03-15 04:20:33 -0700

    create a start new game button

---
+++ src/components/StartNewGameButton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const StartNewGameButton = () => {
+  return (
+    <>
+        <Button>
+            
+        </Button>
+    </>
+  )
+}
+
+export default StartNewGameButton
